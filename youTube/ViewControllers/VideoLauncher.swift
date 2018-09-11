1f1f8068c303eac308479af19f6f6bc720beca95
karthikku <karthik.kumar@citrix.com>
2018-09-11 21:14:51 +0530

    add video player

---
+++ youTube/ViewControllers/VideoLauncher.swift
@@ -0,0 +1,62 @@
+//
+//  VideoLauncher.swift
+//  youtube
+//
+//  Created by Karthik on 11/09/18.
+//  Copyright © 2018 Karthik. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class VideoPlayerView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = UIColor.black
+    
+        if let url = URL(string: "https://www.quirksmode.org/html5/videos/big_buck_bunny.mp4") {
+            let player = AVPlayer(url: url)
+            
+            let playerLayer = AVPlayerLayer(player: player)
+            self.layer.addSublayer(playerLayer)
+            playerLayer.frame = frame
+            
+            player.play()
+        }
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class VideoLauncher {
+    
+    func showVideoPlayer() {
+        
+        if let keyWindow = UIApplication.shared.keyWindow {
+            let view = UIView(frame: keyWindow.frame)
+            view.backgroundColor = UIColor.orange
+            
+            view.frame = CGRect(x: keyWindow.frame.width - 10, y: keyWindow.frame.height - 10, width: 10, height: 10)
+            
+            // 16:9 ratio
+            let height = keyWindow.frame.width * 9 / 16
+            let videoPlayerViewFrame = CGRect(x: 0, y: 0, width: keyWindow.frame.width, height: height)
+            let videoPlayerView = VideoPlayerView(frame: videoPlayerViewFrame)
+            view.addSubview(videoPlayerView)
+            
+            keyWindow.addSubview(view)
+            
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                view.frame = keyWindow.frame
+            }, completion: { (finished) in
+                UIView.animate(withDuration: 0.5, animations: {
+                    UIApplication.shared.isStatusBarHidden = true
+                })
+            })
+        }
+    }
+}
--- youTube/Views/FeedCell.swift
+++ youTube/Views/FeedCell.swift
@@ -66,5 +66,8 @@ class FeedCell: BaseCell, UICollectionViewDataSource, UICollectionViewDelegate, 
         return 0
     }
     
-    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let videoLauncher = VideoLauncher()
+        videoLauncher.showVideoPlayer()
+    }
 }
